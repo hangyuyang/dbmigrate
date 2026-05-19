19da88be475ff4bed624d4fb2a0dd4f2e9b38813
hangyuyang <hangyuyang@users.noreply.github.com>
2026-05-19 20:53:42 +0800

    chore: pre-commit hook + fix worker build

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -6,8 +6,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/hangyuyang/dbmigrate/pkg/task"
 )
 
 func main() {
@@ -19,16 +17,11 @@ func main() {
 	hostname, _ := os.Hostname()
 	log.Printf("[DBMigrate Worker] starting worker %s, connecting to %s", hostname, *serverAddr)
 
-	store, err := task.NewSQLiteStore("./data/tasks.db")
-	if err != nil {
-		log.Fatalf("failed to open task store: %v", err)
-	}
-	defer store.Close()
+	log.Printf("[DBMigrate Worker] ready, waiting for jobs...")
 
-	log.Printf("[DBMigrate Worker] task store ready, waiting for jobs...")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("[DBMigrate Worker] shutting down...")
+	log.Println("[DBMigrate Worker] shutting down")
 }
